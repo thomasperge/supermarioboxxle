14c16d6d6e6f317534efe012823dce9a4fc42cec
Thomasperge <thomaskauffmant74@gmail.com>
2023-04-07 10:00:44 +0200

    feat: add move player

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ import { Levels } from "./level.js";
      38: 'up',
      40: 'down'
  }
+let data = Levels[0]
  
- const draw = () => {
+const draw = () => {
     const container = document.getElementById("gameboard")
 
-    let data = Levels[0]
     for(let i = 0; i < data.length; i++){
         for(let j = 0; j < data[i].length; j++){
             
@@ -43,6 +43,53 @@ import { Levels } from "./level.js";
             }
         }
     }
-    return null
- }
- console.log(draw())
+    return data
+}
+
+function deplacerPersonnage(tableau, direction) {
+    // Get current position of player
+    let pos_x, pos_y;
+    for (let i = 0; i < tableau.length; i++) {
+      for (let j = 0; j < tableau[0].length; j++) {
+        if (tableau[i][j] === 3) {
+          pos_x = i;
+          pos_y = j;
+        }
+      }
+    }
+    
+    // Déplacer le personnage en fonction de la direction
+    if (direction === 38 && pos_x > 0 && tableau[pos_x-1][pos_y] !== 1) {
+      [tableau[pos_x][pos_y], tableau[pos_x-1][pos_y]] = [tableau[pos_x-1][pos_y], tableau[pos_x][pos_y]];
+    } else if (direction === 40 && pos_x < tableau.length-1 && tableau[pos_x+1][pos_y] !== 1) {
+      [tableau[pos_x][pos_y], tableau[pos_x+1][pos_y]] = [tableau[pos_x+1][pos_y], tableau[pos_x][pos_y]];
+    } else if (direction === 37 && pos_y > 0 && tableau[pos_x][pos_y-1] !== 1) {
+      [tableau[pos_x][pos_y], tableau[pos_x][pos_y-1]] = [tableau[pos_x][pos_y-1], tableau[pos_x][pos_y]];
+    } else if (direction === 39 && pos_y < tableau[0].length-1 && tableau[pos_x][pos_y+1] !== 1) {
+      [tableau[pos_x][pos_y], tableau[pos_x][pos_y+1]] = [tableau[pos_x][pos_y+1], tableau[pos_x][pos_y]];
+    }
+    
+    // Retourner le tableau modifié
+    console.log(tableau);
+    return tableau;
+}
+  
+
+document.addEventListener("keydown", function(event) {
+    switch (event.keyCode) {
+      case 38: // Flèche haut
+        deplacerPersonnage(data, 38)
+        break;
+      case 40: // Flèche bas
+        deplacerPersonnage(data, 40)
+        break;
+      case 37: // Flèche gauche
+        deplacerPersonnage(data, 37)
+        break;
+      case 39: // Flèche droite
+        deplacerPersonnage(data, 39)
+        break;
+    }
+});
+
+requestAnimationFrame(draw())
